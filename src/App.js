cc6e259d3c88b9ac143d671b9c7fd02011991b49
Keith Ng <keith.jk.ng@gmail.com>
2020-04-16 18:41:16 -0700

    Attempt to add login capability

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
 import React, { Component } from 'react';
+import MasterPage from './MasterPage';
+import LoginPage from './LoginPage';
+import RegistrationPage from './RegistrationPage';
+import ProfilePage from './ProfilePage';
 import Header from './Header';
 import Books from './Books';
 import logo from './logo.svg';
 import './App.css';
+
+import ReactDOM from 'react-dom';
+import { IndexRoute, Route } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history'
+const history = createBrowserHistory();
+;
+
+ReactStormpath.init();
+ReactDOM.render(
+  <Router history={history}>
+  </Router>,
+  document.getElementById('app-container')
+);
+
+<Router history={history}>
+  <Route path='/' component={MasterPage}>
+    <LoginRoute path='/login' component={LoginPage} />
+    <Route path='/register' component={RegistrationPage} />
+    <AuthenticatedRoute path='/profile' component={ProfilePage} />
+  </Route>
+</Router>
 
 class App extends Component {
   render() {
